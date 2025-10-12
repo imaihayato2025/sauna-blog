41ccdb2eb6b7c41ed7a302098e87b8d595b67d32
imahaya <imaihayato2024@gmail.com>
2025-10-12 09:40:37 +0900

    最初のコミット

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
