e0b5e071c85ae24976fbec9fffec9c182f174909
imahaya <imaihayato2024@gmail.com>
2025-10-16 20:51:40 +0900

    メニューを消してロゴを真ん中に配置

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,9 +3,8 @@ import Nav from "@/components/layout/Nav";
 export default function Header() {
   return (
     <header className="max-w-7xl w-full h-24 flex flex-col justify-center items-center mx-auto">
-      <div className="flex justify-between items-center w-[90%] ">
+      <div className="flex justify-center w-[90%] ">
         <Logo />
-        <Nav />
       </div>
     </header>
   );
