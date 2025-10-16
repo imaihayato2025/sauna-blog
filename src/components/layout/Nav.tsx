8bca1101efd0f8804eba15ff9ab6de45da76c020
imahaya <imaihayato2024@gmail.com>
2025-10-16 21:48:06 +0900

    メニューのアイコン実装

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState } from "react"
-
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,15 +17,21 @@ export default function Nav() {
 
   return (
     <nav className="flex">
-     <button onClick={navToggle} className={`absolute top-5 right-5 w-10 h-10 z-10 bg-red-500 ${isOpen ? "bg-blue-600": "bg-gray-600"}`}></button>
-     <ul className={`flex flex-col justify-center items-center absolute right-0 top-0 bg-black w-64 h-screen ${isOpen ? "translate-x-0": "translate-x-full"}`}>
-      {navItems.map((item) => (
-        <li key={item.label} className="text-white">{item.label}</li>
-      ))}
-     </ul>
+      <button
+        onClick={navToggle}
+        className={`absolute top-5 right-5 z-10 h-7 w-7 bg-red-500 ${isOpen ? "bg-blue-600" : "bg-gray-600"}`}
+      >
+        {isOpen ? <HiX size={28} /> : <HiMenu size={28} />}
+      </button>
+      <ul
+        className={`absolute top-0 right-0 flex h-screen w-64 flex-col items-center justify-center bg-black ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        {navItems.map((item) => (
+          <li key={item.label} className="text-white">
+            {item.label}
+          </li>
+        ))}
+      </ul>
     </nav>
-
-
-
   );
 }
