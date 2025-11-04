fc2f0735366c0071b99707c9fc0473c3708d2bb0
imahaya <imaihayato2024@gmail.com>
2025-11-04 21:30:57 +0900

    記事詳細ページのデータの取得方法の修正とデザインを更新

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,16 +1,20 @@
 import { client } from "@/libs/microcms";
+import Image from "next/image";
 
-type BlogPost = {
-  params: Promise<{ id: string }>;
+type ArticleProps = {
   id: string;
+  category: string;
   title: string;
+  eyecatch?: {
+    url: string;
+  };
 };
 
 type Params = {
   id: string;
 };
 
-function getBlogPost(id: string): Promise<BlogPost> {
+function getBlogPost(id: string): Promise<ArticleProps> {
   const data = client.get({
     endpoint: "saunablog",
     contentId: id,
@@ -24,10 +28,21 @@ export default async function BlogPostPage({
   params: Promise<Params>;
 }) {
   const { id } = await params;
-  const post = await getBlogPost(id);
+  const article = await getBlogPost(id);
   return (
-    <div>
-      <div key={post.id}>{post.title}</div>
+    <div className="mx-auto w-[90%] max-w-5xl md:w-1/2">
+      <div className="relative mb-4 aspect-[16/9] w-full">
+        <Image
+          src={article.eyecatch?.url || "/noimage.png"}
+          alt={article.title}
+          fill
+          className="rounded-md object-cover"
+        />
+      </div>
+      <span className="bg-black px-1 py-0.5 text-base text-white">
+        {article.category}
+      </span>
+      <h3 className="pb-8 text-left text-2xl font-bold">{article.title}</h3>
     </div>
   );
 }
