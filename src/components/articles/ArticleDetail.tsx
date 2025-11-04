ab1f8531624a04d6b1225124716baf2cc8f31e0a
imahaya <imaihayato2024@gmail.com>
2025-11-04 20:47:49 +0900

    記事にリンクを追加

--- src/components/articles/ArticleDetail.tsx
+++ src/components/articles/ArticleDetail.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type ArticleDetailProps = {
   id: string;
@@ -16,27 +17,29 @@ export default function ArticleDetail({
   date,
 }: ArticleDetailProps) {
   return (
-    <div className="flex w-full border-b border-[#dadce0] pb-4 sm:w-[48%] sm:flex-col md:w-[31%]">
-      <div className="relative aspect-[16/9] w-[35%] sm:w-full">
-        <Image
-          src={eyecatch}
-          alt={title}
-          fill
-          className="rounded-md object-cover"
-        />
-      </div>
-
-      <div className="w-[65%] p-2 sm:w-full">
-        <div className="flex items-center gap-2 text-[#949497]">
-          <span className="bg-black px-1 text-[10px] text-white">
-            {category}
-          </span>
-          <span className="text-[10px]">{date}</span>
+    <Link href={`/articles/${id}`}>
+      <div className="flex w-full border-b border-[#dadce0] pb-4 sm:w-[48%] sm:flex-col md:w-[31%]">
+        <div className="relative aspect-[16/9] w-[35%] sm:w-full">
+          <Image
+            src={eyecatch}
+            alt={title}
+            fill
+            className="rounded-md object-cover"
+          />
         </div>
-        <h3 className="text-[clamp(0.75rem, 0.667rem + 0.42vw, 1rem)] font-bold">
-          {title}
-        </h3>
+
+        <div className="w-[65%] p-2 sm:w-full">
+          <div className="flex items-center gap-2 text-[#949497]">
+            <span className="bg-black px-1 text-[10px] text-white">
+              {category}
+            </span>
+            <span className="text-[10px]">{date}</span>
+          </div>
+          <h3 className="text-[clamp(0.75rem, 0.667rem + 0.42vw, 1rem)] font-bold">
+            {title}
+          </h3>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
